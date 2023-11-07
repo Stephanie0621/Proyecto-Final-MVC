e2f74d00d150c653648e435e03906a86f6620689
Stephanie0621 <stephanie.maldotizo@gmail.com>
2023-11-07 09:32:31 -0600

    Proyecto

---
+++ MvcApplication1/Models/Order.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace MvcApplication1.Models
+{
+    [Bind(Exclude = "OrderId")]
+    public partial class Order
+    {
+        [ScaffoldColumn(false)]
+        public int OrderId { get; set; }
+
+        [ScaffoldColumn(false)]
+        public System.DateTime OrderDate { get; set; }
+
+        [ScaffoldColumn(false)]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "First Name is required")]
+        [DisplayName("Primer Nombre")]
+        [StringLength(160)]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last Name is required")]
+        [DisplayName("Apellido")]
+        [StringLength(160)]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Address is required")]
+        [DisplayName("Dirección")]
+        [StringLength(70)]
+        public string Address { get; set; }
+
+        [Required(ErrorMessage = "City is required")]
+        [DisplayName("Ciudad")]
+        [StringLength(40)]
+        public string City { get; set; }
+
+        [Required(ErrorMessage = "State is required")]
+        [DisplayName("Estado")]
+        [StringLength(40)]
+        public string State { get; set; }
+
+        [Required(ErrorMessage = "Postal Code is required")]
+        [DisplayName("Código Postal")]
+        [StringLength(10)]
+        public string PostalCode { get; set; }
+
+        [Required(ErrorMessage = "Country is required")]
+        [DisplayName("País")]
+        [StringLength(40)]
+        public string Country { get; set; }
+
+        [Required(ErrorMessage = "Phone is required")]
+        [DisplayName("Teléfono")]
+        [StringLength(24)]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Email Address is required")]
+        [DisplayName("Correo Electrónico")]
+        [RegularExpression(@"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}",
+            ErrorMessage = "Email is is not valid.")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [ScaffoldColumn(false)]
+        public decimal Total { get; set; }
+
+        public List<OrderDetail> OrderDetails { get; set; }
+    }
+}
---
+++ MvcApplication1/ViewModels/ShoppingCartViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using MvcApplication1.Models;
+using System.Linq;
+using System.Web;
+
+namespace MvcApplication1.ViewModels
+{
+    public class ShoppingCartViewModel
+    {
+        public List<Cart> CartItems { get; set; }
+        public decimal CartTotal { get; set; }
+    }
+}
